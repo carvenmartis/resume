ea004d1b2763db5f5594b5b9fc097f978b11f3bc
Carven Martis <martens-07.jollity@icloud.com>
2024-12-20 21:20:44 +0100

    contact

---
+++ src/pages/contact.tsx
@@ -0,0 +1,29 @@
+import { ContactProps } from "@/types/resume";
+import React from "react";
+
+const Contact = ({ contact }: { contact: ContactProps }) => {
+  return (
+    <div>
+      <div className="flex items-center gap-3 pt-[6rem]">
+        <div className="p-2 bg-[#8ea59b] text-white rounded-full flex justify-center items-center">
+          <i className="bx bxs-map"></i>
+        </div>
+        <p className="">{contact.location}</p>
+      </div>
+      <div className="flex items-center gap-3">
+        <div className="p-2 theme-bg-color text-white rounded-full flex justify-center items-center">
+          <i className="bx bxs-phone"></i>
+        </div>
+        <p className="">{contact.phone}</p>
+      </div>
+      <div className="flex items-center gap-3">
+        <div className="p-2 theme-bg-color text-white rounded-full flex justify-center items-center">
+          <i className="bx bxs-envelope "></i>
+        </div>
+        <p className="">{contact.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
