c6dd07ee32f37228226842e838d91253bdeb1650
Carven Martis <martens-07.jollity@icloud.com>
2024-12-21 14:34:37 +0100

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = ({ pageNumber }: { pageNumber: number }) => {
   return (
-    <footer className="flex bg-purple-600 w-full justify-end pr-[12rem]">
+    <footer className="h-[12rem] flex justify-end pr-[12rem]">
       <p className="text-[2.5rem] text-gray-600">{pageNumber}</p>
     </footer>
   );
