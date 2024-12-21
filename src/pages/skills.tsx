c7076fc17c54ff705ad9bb57178b2bb95aa795eb
Carven Martis <martens-07.jollity@icloud.com>
2024-12-21 22:06:22 +0100

    skills now fill up empty space

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -2,13 +2,19 @@ import React from "react";
 
 const Skills = ({ skills }: { skills: string[] }) => {
   return (
-    <div className="w-[42.5rem]">
+    <div>
       <h3 className="text-black">Skills</h3>
       <div className="flex flex-wrap gap-3">
         {skills.map((skill, index) => (
           <span
             key={index}
-            className="p-3 bg-[#c6d1cd] text-gray-700 text-[2.4rem] truncate rounded-md"
+            className={`p-3 bg-[#c6d1cd] text-gray-700 text-[2.4rem] rounded-md flex-none text-center ${
+              index === 0
+                ? "ml-auto"
+                : index === skills.length - 1
+                ? "mr-auto"
+                : "flex-grow"
+            }`}
           >
             {skill}
           </span>
