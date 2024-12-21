5d23b028740029c67a98381fbba1a66907b9490d
Carven Martis <martens-07.jollity@icloud.com>
2024-12-21 11:29:32 +0100

    language item

--- src/components/language-item.tsx
+++ src/components/language-item.tsx
@@ -5,12 +5,12 @@ const LanguageItem = ({ language }: { language: LanguageItemProps }) => {
   return (
     <li className="text-gray-700 text-justify text-2lg text-[2.4rem] w-full">
       <div className="flex items-center justify-between w-full">
-        <span className="font-bold"> {language.title}</span>
+        <p> {language.title}</p>
         <div className="flex mr-10 gap-4">
           {[...Array(5)].map((_, index) => (
             <span
               key={index}
-              className={`w-8 h-8 rounded-full mx-1 ${
+              className={`w-10 h-10 rounded-full mx-1 ${
                 index < language.level ? "bg-[#8ea59b]" : "bg-[#c6d1cd]"
               }`}
             ></span>
