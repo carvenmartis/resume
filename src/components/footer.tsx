e62a70569b8d623999401a1fe7398ad19fff94f6
Carven Martis <martens-07.jollity@icloud.com>
2024-12-21 12:15:21 +0100

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = ({ pageNumber }: { pageNumber: number }) => {
   return (
-    <div className="mx-auto w-[80%] flex justify-end">
+    <div className="w-[94.5%] flex justify-end">
       <p className="text-[2.5rem] text-gray-600">{pageNumber}</p>
     </div>
   );
