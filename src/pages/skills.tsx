27499fdd724199c283e9a678767d755a319b5579
Carven Martis <martens-07.jollity@icloud.com>
2024-12-21 11:43:38 +0100

    align

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -4,7 +4,7 @@ const Skills = ({ skills }: { skills: string[] }) => {
   return (
     <div>
       <h3 className="text-black">Skills</h3>
-      <div className="w-[42rem]">
+      <div className="w-[42.3rem]">
         <span className="text-gray-700 text-justify text-2lg text-[2.4rem] leading-snug">
           {skills.join(", ")}
         </span>
